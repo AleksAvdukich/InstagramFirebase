7ef3173c32e53343b2256d1f62be0d24bc6c7572
Aleksandr Avdukich <avdukichsanel@mail.ru>
2018-12-17 04:11:15 +0300

    Added realization user interface of the main tab added and user selection

---
+++ InstagramFirebase/MainTabBarController.swift
@@ -0,0 +1,27 @@
+//
+//  MainTabBarController.swift
+//  InstagramFirebase
+//
+//  Created by Aleksandr Avdukich on 17/12/2018.
+//  Copyright © 2018 Sanel Avdukich. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let layout = UICollectionViewFlowLayout()
+        let userProfileController = UserProfileController(collectionViewLayout: layout)
+        
+        let navController = UINavigationController(rootViewController: userProfileController)
+        
+        navController.tabBarItem.image = UIImage(named: "profile_unselected")
+        navController.tabBarItem.selectedImage = UIImage(named: "profile_selected")
+        
+        tabBar.tintColor = .black
+        
+        viewControllers = [navController, UIViewController()]
+    }
+}
