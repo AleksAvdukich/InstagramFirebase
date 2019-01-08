ff18c938a93f468b3088e139700766f2a84c569b
Aleksandr Avdukich <avdukichsanel@mail.ru>
2019-01-08 11:13:03 +0300

    Insert Comments

--- InstagramFirebase/Home/HomeController.swift
+++ InstagramFirebase/Home/HomeController.swift
@@ -85,7 +85,8 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
                 
                 guard let dictionary = value as? [String: Any] else { return }
                 
-                let post = Post(user: user, dictionary: dictionary)
+                var post = Post(user: user, dictionary: dictionary)
+                post.id = key
                 
                 self.posts.append(post)
             })
@@ -143,6 +144,7 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
         print("Message coming from HomeController")
         print(post.caption)
         let commentsController = CommentsController(collectionViewLayout: UICollectionViewFlowLayout())
+        commentsController.post = post
         navigationController?.pushViewController(commentsController, animated: true)
     }
 }
